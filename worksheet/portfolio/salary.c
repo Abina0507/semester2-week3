3a3f19109bf402574538ad8c9b3cd1506341221d
Abina0507 <a-alansari@outlook.com>
2026-02-23 15:48:45 +0000

    Testing#1

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -19,5 +19,24 @@
     //printf("Tax contribution £...",var_name);
     //printf("Take home salary £...",var_name);
 
+  int salary = 36250;
+	int NIrate = 8;
+	int taxRate = 15;	
+	
+	float NIContribution =  salary * NIRate / 100; //2,900
+	float takeHomeSalary = salary - NIContribution; //33,350
+	
+	
+	float taxContribution = (takeHomeSalary - 12500) * taxRate / 100; //3,127.5
+	
+	takeHomeSalary = takeHomeSalary - taxContribution; //30,222.5
+	
+	printf("Salary £...",salary);
+    printf("NI contribution £",NIContribution);
+    printf("Tax contribution £...",taxContribution);
+    printf("Take home salary £...",takeHomeSalary);
+
+    return 0;
+
     return 0;
  }
